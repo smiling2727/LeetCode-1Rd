28c6154dced7117bb1a66f0dce6298ec49df340a
smiling2727 <xinyuel527@gmail.com>
2016-05-17 11:28:31 +0800

    Update AddDigits_258.java

--- AddDigits_258.java
+++ AddDigits_258.java
@@ -1,13 +1,9 @@
 public class Solution {
     public int addDigits(int num) {
-       int n = num;
-       while((n/10)>0){
-       String str =  String.valueOf(n);
-       char[] ch = str.toCharArray();
-       for(int i=0;i<ch.length;i++){
-           n = n+Integer.parseInt(String.valueOf(ch[i]));
-            }
+       int n =9;
+       if(num==0||num%9!=0){
+            n= num%9;
         }
-       return n;
+    return n;
     }
 }
