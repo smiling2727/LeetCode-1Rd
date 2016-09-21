f5ea321294b1266c4831bed85330fbdd3999f10b
smiling2727 <xinyuel527@gmail.com>
2016-09-20 23:53:37 -0400

    Update and rename AddTwoNumbers_2 to AddTwoNumbers_2.java

--- AddTwoNumbers_2.java
+++ AddTwoNumbers_2.java
@@ -8,37 +8,27 @@
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode temp ;
-        if(getSize(l1)<getSize(l2)) {
-            temp = l1;
-            l1 = l2;
-            l2 =temp;
-        }
-        temp = l1;
-        while(l2!=null){//loop the small one
+        ListNode temp1 = l1 ;
+        ListNode temp2 = l2 ;
+        while(l2!=null&&l1!=null){//loop the small one
             if(l1.val + l2.val<10){
                 l1.val =l1.val+l2.val;
+                l2.val =l1.val;
                 l1 = l1.next;
                 l2 = l2.next;
             }
             else{
                 l1.val =(l1.val+l2.val)%10;
+                l2.val =l1.val;
                 moveDigit(l1);
+                //moveDigit(l2);
                 l1 = l1.next;
                 l2 = l2.next;
             }
         }
-        return temp;
+         return l1!=null?temp1:temp2;
     }
     
-    public int getSize(ListNode l){
-        int size=0;
-        while(l!=null){
-            size++;
-            l = l.next;
-        }
-        return size;
-    }
     public void moveDigit(ListNode n){
         if(n.next==null) {
             n.next = new ListNode(1);
