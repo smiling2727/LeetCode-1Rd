f0aa02c207fe90061f159be4b67a9e6012f56075
smiling2727 <xinyuel527@gmail.com>
2016-10-06 23:12:29 -0400

    Update RepeatedDNA_187.java

--- RepeatedDNA_187.java
+++ RepeatedDNA_187.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class repeatedDNA_187 {
+public class repeatedDNA187 {
     /**
      * Created by Smiling on 10/6/16.
      */
@@ -8,51 +8,39 @@ public class repeatedDNA_187 {
         BSTree tree = new BSTree();
         List<String> rpList = new ArrayList<String>();
 
-        for(int i=0;i<s.length()-9;i++){
+        for(int i=0;i<=s.length()-10;i++){
             String subStr = s.substring(i,i+10);
-            //System.out.println(subStr);
+            System.out.println(subStr);
             tree.insert(subStr);
         }
         System.out.println("rpSet.size = "+tree.rpSet.size());
         Iterator it = tree.rpSet.iterator();
+
 //        while(it.hasNext()){
 //            //String addstr = it.next();
 //            rpList.add((String)it.next());
 //        }
 
-        for(String addStr:tree.rpSet){ //这种和上面那个iterator完全一样
-            rpList.add(addStr);
-        }
+//        for(String addStr:tree.rpSet){ //这种和上面那个iterator完全一样
+//            rpList.add(addStr);
+//        }
 
-        return rpList;
+        return new ArrayList<>(tree.rpSet);//把set直接变成list
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        repeatedDNA187 r = new repeatedDNA_187();
+        repeatedDNA187 r = new repeatedDNA187();
         String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        //String s = "AAAAAAAAAAA";
         List list = r.findRepeatedDnaSequences(s);
-//        System.out.println("list.size = "+list.size());
-        //System.out.println(s.compareTo("ACAAACACCCAAAAACCCCCCAAAAAGGGTTT"));
 
-//        BSTree BST = new BSTree();
-//        String s1 = new String("AAAAACCCCC");
-//        //String s2 = new String("AAAAACCCCC");
-//        String s3 = new String("CCCCCAAAAA");
-//        String s4 = new String("CCCCCAAAAA");
-//        System.out.println(s1.compareTo(s3));
-//        System.out.println(s3.compareTo(s4));
-//        BST.insert(s1);
-//        //BST.insert(s2);
-//        BST.insert(s3);
-//        BST.insert(s4);
     }
 
 }
 
 class BSTree{
     private BSTreeNode root;
-    //protected List<String> replist = new ArrayList<String>();
     protected Set<String> rpSet = new HashSet<String>();
 
     public void insert(String str){
