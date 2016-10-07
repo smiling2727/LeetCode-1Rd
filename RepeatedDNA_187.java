11913d342d9abb8acf6f1e1afbeb73dd8b34350d
smiling2727 <xinyuel527@gmail.com>
2016-10-06 23:31:53 -0400

    Update RepeatedDNA_187.java

--- RepeatedDNA_187.java
+++ RepeatedDNA_187.java
@@ -1,86 +1,17 @@
-import java.util.*;
-
-public class repeatedDNA187 {
-    /**
-     * Created by Smiling on 10/6/16.
-     */
+public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        BSTree tree = new BSTree();
-        List<String> rpList = new ArrayList<String>();
-
-        for(int i=0;i<=s.length()-10;i++){
+        Set<String> seen = new HashSet<String>();
+        Set<String> repSet = new HashSet<String>();
+        
+        for(int i=0;i<s.length()-9;i++){
             String subStr = s.substring(i,i+10);
-            System.out.println(subStr);
-            tree.insert(subStr);
+            if(seen.contains(subStr)){
+                repSet.add(subStr);
+            }
+            else seen.add(subStr);
+            
         }
-        System.out.println("rpSet.size = "+tree.rpSet.size());
-        Iterator it = tree.rpSet.iterator();
-
-//        while(it.hasNext()){
-//            //String addstr = it.next();
-//            rpList.add((String)it.next());
-//        }
-
-//        for(String addStr:tree.rpSet){ //这种和上面那个iterator完全一样
-//            rpList.add(addStr);
-//        }
-
-        return new ArrayList<>(tree.rpSet);//把set直接变成list
-    }
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        repeatedDNA187 r = new repeatedDNA187();
-        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
-        //String s = "AAAAAAAAAAA";
-        List list = r.findRepeatedDnaSequences(s);
-
-    }
-
-}
-
-class BSTree{
-    private BSTreeNode root;
-    protected Set<String> rpSet = new HashSet<String>();
-
-    public void insert(String str){
-        root = insert(root,str);
-    }
-
-    public BSTreeNode insert(BSTreeNode root, String str){//insert with search
-        if(root==null){
-            root = new BSTreeNode(str);//构造新的子树（树）
-            //System.out.println("insert "+ str);
-            return root;
-        }
-
-        if(str.compareTo(root.data)<0) {
-            root.left = insert(root.left,str);}
-        else if(str.compareTo(root.data)>0){
-            root.right = insert(root.right,str);}
-        else{
-            rpSet.add(str);
-            System.out.println("equal "+str);
-        }
-        return root;
-    }
-
-
-}
-class BSTreeNode{
-    protected String data;
-    protected BSTreeNode left;
-    protected BSTreeNode right;
-
-    public BSTreeNode(){
-    }
-    public BSTreeNode(String data){
-        this.data = data;
-    }
-    public BSTreeNode(BSTreeNode left,BSTreeNode right,String data){
-        this.left = left;
-        this.right = right;
-        this.data = data;
+        return new ArrayList<>(repSet);
     }
 }
 
