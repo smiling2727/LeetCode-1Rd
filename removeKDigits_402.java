bac53cd9f00cb988c8ab2553a0c2c4f1120d5419
smiling2727 <xinyuel527@gmail.com>
2016-10-23 22:18:52 -0400

    Update removeKDigits_402.java

--- removeKDigits_402.java
+++ removeKDigits_402.java
@@ -2,11 +2,11 @@ public class Solution {
     public String removeKdigits(String num, int k) {
         int counter = 0;
         String num1 = helper(num,k);
-        if(num1.length()==0) return "0";
+       // if(num1.length()==0) return "0";
         char[] ch = num1.toCharArray();
         for(int i=0;i<ch.length;i++){
-            if(ch[i]=='0') counter++;
             if(ch[i]!='0') break;
+            counter++;
         }
         String num2 = num1.substring(counter,num1.length());
         if(num2.length()==0) return "0";
@@ -20,15 +20,15 @@ public class Solution {
         char min = num.charAt(0); 
         int minIndex  = 0;
         char[] ch = num.toCharArray();
-        for(int i=0;i<length-k;i++){
-        //for(int i=0;i<k;i++){
+        //for(int i=0;i<length-k;i++){
+        for(int i=0;i<=k;i++){
             if(ch[i]<min){
                 min = ch[i];
                 minIndex = i;
             }
         }
-       String num2 =  helper(num.substring(minIndex+1,length),k-1);
-       //String num2 =  helper(num.substring(minIndex+1,length),k-minIndex);
+       //String num2 =  helper(num.substring(minIndex+1,length),k-1);
+       String num2 =  helper(num.substring(minIndex+1,length),k-minIndex);
        return num.charAt(minIndex)+num2;
     }
 }
