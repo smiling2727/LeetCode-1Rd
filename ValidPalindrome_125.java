d9b73b5c734aff06d80032a12cf44fb4a46c54ea
smiling2727 <xinyuel527@gmail.com>
2016-10-24 20:52:31 -0400

    Update ValidPalindrome_125.java

--- ValidPalindrome_125.java
+++ ValidPalindrome_125.java
@@ -1,17 +1,12 @@
 import java.util.regex.*;
 public class Solution {
     public boolean isPalindrome(String s) {
-     String strPattern = "[a-zA-Z0-9]+";
-     StringBuilder sbuilder = new StringBuilder();
-     Pattern p = Pattern.compile(strPattern);
-     Matcher m = p.matcher(s);
-     while(m.find()){
-         sbuilder.append(m.group());
-     }
-     String str = sbuilder.toString().toLowerCase();
+     String strPattern = "[^a-zA-Z0-9]+";
+     Matcher m = Pattern.compile(strPattern).matcher(s);
+     String str = m.replaceAll("").toLowerCase();
      if(str.length()==0) return true;
      char[] ch = str.toCharArray();
-     for(int i=0;i<=(int)(ch.length/2);i++){
+     for(int i=0;i<=ch.length/2;i++){
              if(ch[i]!=ch[ch.length-1-i]) return false;
          }
      return true;
