4d470fe0d0590c5893f6738881499fbf73d0c24f
smiling2727 <xinyuel527@gmail.com>
2016-10-28 22:57:03 -0400

    Update ZigZagConversion_6.java

--- ZigZagConversion_6.java
+++ ZigZagConversion_6.java
@@ -2,27 +2,27 @@ public class Solution {
     public String convert(String s, int numRows) {
         if(s.length()==0||numRows < 2) return s;
         char[] sch = s.toCharArray();
-        char[][] ch = new char[s.length()][numRows];
         int index = 0;
         StringBuilder sbd = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            if (i % 2 == 0) {
-                for (int j = 0; j < numRows; j++) {
-                    if (index < s.length()) {
-                        ch[i][j] = sch[index];
-                        index++;
+        for(int row =0;row<numRows;row++){
+            if(row==0||row==numRows-1){
+                for(int i = row;i<s.length();i=i+2*(numRows-1)){
+                    sbd.append(s.charAt(i));
+                }
+            }
+            else{
+                int i = row;
+                int col = 0;
+                while(i<s.length()){
+                    sbd.append(s.charAt(i));
+                    col++;
+                    if(col%2==1){
+                        i=i+2*(numRows-row-1);
+                    }
+                    else{
+                       i=i+2*row; 
                     }
                 }
-            } else {
-                    for (int j = numRows-2; j >0 ; j--) {
-                        if (index < s.length()) {
-                            ch[i][j] = sch[index];
-                            index++;}}}}
-
-        for (int j = 0; j < numRows; j++) {
-            for (int i = 0; i < s.length(); i++) {
-                if (ch[i][j] != '\0') sbd.append(ch[i][j]);
             }
         }
         return sbd.toString();
